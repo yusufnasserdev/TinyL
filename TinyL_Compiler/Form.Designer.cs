45a0619c499acd0cb97a83371227a855b9c4fc92
Steven Sameh Soliman <42946298+stevensameh@users.noreply.github.com>
2021-11-15 23:19:16 +0200

    Created TinyL_Compiler project
    Co-Authored-By: Mostafa-Moharram <93437103+Mostafa-Moharram@users.noreply.github.com>
    Co-Authored-By: Nada Adel <79041556+Nada57246@users.noreply.github.com>
    Co-Authored-By: Youssef Nasser <yesyosife@gmail.com>

---
+++ TinyL_Compiler/Form.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TinyL_Compiler
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CompileButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CompileButton
+            // 
+            this.CompileButton.Location = new System.Drawing.Point(69, 381);
+            this.CompileButton.Name = "CompileButton";
+            this.CompileButton.Size = new System.Drawing.Size(94, 29);
+            this.CompileButton.TabIndex = 0;
+            this.CompileButton.Text = "Compile";
+            this.CompileButton.UseVisualStyleBackColor = true;
+            this.CompileButton.Click += new System.EventHandler(this.CompileButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(535, 381);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(94, 29);
+            this.ClearButton.TabIndex = 1;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CompileButton);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button CompileButton;
+        private Button ClearButton;
+    }
+}
---
+++ TinyL_Compiler/Form.cs
@@ -0,0 +1,20 @@
+namespace TinyL_Compiler
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void CompileButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
